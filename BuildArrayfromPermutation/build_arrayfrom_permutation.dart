f0c638ef15ebaac53003fd87b16551a82bc14233
sadiksaroar <sadiksaroar275@gmail.com>
2026-08-02 09:34:47 +0600

    Build Array from Permutation

--- BuildArrayfromPermutation/build_arrayfrom_permutation.dart
+++ BuildArrayfromPermutation/build_arrayfrom_permutation.dart
@@ -1,3 +1,24 @@
+/*void main() {
+  List<int> nums = [0, 2, 1, 5, 3, 4];
+  print((nums));
+}
+*/
+
+class Solution {
+  List<int> buildArray(List<int> nums) {
+    List<int> ans = List.filled(nums.length, 0);
+
+    for (int i = 0; i < nums.length; i++) {
+      ans[i] = nums[nums[i]];
+    }
+
+    return ans;
+  }
+}
+
 void main() {
-  print("Build Array from Permutation");
+  List<int> nums = [5, 0, 1, 2, 3, 4];
+
+  Solution solution = Solution();
+  print(solution.buildArray(nums));
 }
